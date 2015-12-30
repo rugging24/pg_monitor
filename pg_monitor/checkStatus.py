102f0e9ab11b95cfe28cb47e73516256f8e53d7e
Olakunle Olaniyi <olakunle.olaniyi@billpay.de>
2015-12-30 20:58:41 +0100

    fixed check Status

--- pg_monitor/checkStatus.py
+++ pg_monitor/checkStatus.py
@@ -2,11 +2,11 @@
 
 
 def getStatus(V,Wn,Cr,Mn=0,Mx=0) :
-	if V < Wn and Wn < Cr and Mn <= V : #and V < Mx and Cr <= Mx and Wn >= Mn :
+	if V < Wn and Wn < Cr  : #and V < Mx and Cr <= Mx and Wn >= Mn :
 		return 0
-	elif Wn <= V and V < Cr and Mn <= V : #and V < Mx and Cr <= Mx and Wn >= Mn :
+	elif Wn <= V and V < Cr : #and V < Mx and Cr <= Mx and Wn >= Mn :
 		return 1
-	elif Wn < Cr and Cr <= V  and Wn >= Mn :
+	elif Wn <= Cr and Cr <= V  :
 		return 2
 	else :
 		return 3
