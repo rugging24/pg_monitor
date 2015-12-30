20cef77da320ebbe6d5d78359bceb940aaa25e89
Olakunle Olaniyi <olakunle.olaniyi@billpay.de>
2015-12-30 19:59:36 +0100

    added missing import

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
         return f.read()
 
 setup(name='pg_monitor',
-      version='1.1',
+      version='1.2',
       description='PostgreSQL monitoring checks',
       long_description=readme(),
       classifiers=[
