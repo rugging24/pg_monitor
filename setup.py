f298922510eeaca314c4f1ba32687975278aa8c4
Olakunle Olaniyi <olakunle.olaniyi@billpay.de>
2015-12-30 18:53:39 +0100

    readme and setuptools

--- setup.py
+++ setup.py
@@ -2,9 +2,20 @@
 
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='pg_monitor',
       version='1.0',
       description='PostgreSQL monitoring checks',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Networking :: Monitoring',
+      ],
       url='https://github.com/rugging24/pg_monitor',
       author='Olakunle Olaniyi',
       author_email='rugging24@gmail.com',
@@ -13,4 +24,5 @@ setup(name='pg_monitor',
       install_requires=[
           'psycopg2',
       ],
+      include_package_data=True,
       zip_safe=False)
