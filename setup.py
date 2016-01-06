1abf014e17a5ed7168be648aa1bc5a5212e23647
Olakunle Olaniyi <olakunle.olaniyi@billpay.de>
2016-01-07 00:52:17 +0100

    fixed replica lag

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
         return f.read()
 
 setup(name='pg_monitor',
-      version='1.4.21',
+      version='1.4.22',
       description='PostgreSQL monitoring checks',
       long_description=readme(),
       classifiers=[
