1f8557792cd3ddda4f50ed1ed2515426de13f4b3
Olakunle Olaniyi <olakunle.olaniyi@billpay.de>
2016-01-12 15:10:08 +0100

    bloats

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
         return f.read()
 
 setup(name='pg_monitor',
-      version='1.4.27',
+      version='1.4.28',
       description='PostgreSQL monitoring checks',
       long_description=readme(),
       classifiers=[
