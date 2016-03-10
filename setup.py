751373b292fec80946e7cfcb9d9a972fca57ba49
Olakunle Olaniyi <olakunle.olaniyi@billpay.de>
2016-03-10 13:05:10 +0100

    fixing critical values

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
         return f.read()
 
 setup(name='pg_monitor',
-      version='1.4.48',
+      version='1.4.49',
       description='PostgreSQL monitoring checks',
       long_description=readme(),
       classifiers=[
