53ec74f7d8de7fc1618a9ad72543eef891aae8cb
Olakunle Olaniyi <olakunle.olaniyi@billpay.de>
2016-03-19 15:24:47 +0100

    fixed Invalid conversion specification for blocking checks

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
         return f.read()
 
 setup(name='pg_monitor',
-      version='1.5.6',
+      version='1.5.7',
       description='PostgreSQL monitoring checks',
       long_description=readme(),
       classifiers=[
