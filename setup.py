2dfa662f23294207550358f4970236a068f3d17a
Olakunle Olaniyi <olakunle.olaniyi@billpay.de>
2016-03-29 17:20:05 +0200

    version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
         return f.read()
 
 setup(name='pg_monitor',
-      version='1.6.9',
+      version='1.7.0',
       description='PostgreSQL monitoring checks',
       long_description=readme(),
       classifiers=[
